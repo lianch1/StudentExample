7436f8c0cb0198af16ac6e53ee6c0c3a41d1b25d
xiaoh <xiaoh@192.168.2.15>
2021-02-02 10:23:11 -0500

    My first commit

---
+++ src/tutoring/Tutoring1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutoring;
+
+/**
+ *
+ * @author xiaoh
+ */
+public class Tutoring1 {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            double radius=2.0;
+            double pie=3.14;
+        System.out.println("Area of my circle is: " + pie*radius*radius);
+    }
+    
+}
